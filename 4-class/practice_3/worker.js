1cb2ac7b564b8a1ad74e5475df2a48190b54ea3b
crystalfn <969274162@qq.com>
2020-09-21 21:19:28 +0800

    [fangni] feat: implement class - practice_4

--- 4-class/practice_3/worker.js
+++ 4-class/practice_3/worker.js
@@ -1,14 +1,7 @@
 // TODO 14: 在这里写实现代码
-
 import Person from './person';
 
 export default class Worker extends Person {
-  // eslint-disable-next-line no-useless-constructor
-  constructor(name, age) {
-    super(name, age);
-  }
-
-  // eslint-disable-next-line class-methods-use-this
   introduce() {
     return 'I am a Worker. I have a job.';
   }
